69910c05d0b7da900b458f28bdae04b1f3b3e816
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-28 02:35:53 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -8,7 +8,30 @@ module Memorable
     def count
       self.all.count
     end
+    
+    
+    module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+ 
+    def count
+      self.all.count
+    end
   end
+ 
+  module InstanceMethods
+    def initialize
+      # some more code coming soon!
+    end
+  end
+  
+end
+    
+    
+  end
+  
 
 
 end
